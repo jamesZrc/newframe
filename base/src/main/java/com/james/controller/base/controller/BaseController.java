c8aa470797a38c403ba91193d0c5a8a9883b08b2
Rongchang Zeng <rongchang.zeng@ericsson.com>
2016-11-01 18:25:54 +0800

    Others#24134, q34r added
    Change-Id: If26c099de09ab1ba2155ce778bbfdadec3adee31

--- base/src/main/java/com/james/controller/base/controller/BaseController.java
+++ base/src/main/java/com/james/controller/base/controller/BaseController.java
@@ -1,15 +1,12 @@
 package com.james.controller.base.controller;
 
-import org.springframework.stereotype.Controller;
+import org.apache.log4j.Logger;
 
 /**
  * Created by eronzen on 10/28/2016.
  */
-@Controller
 public class BaseController {
 
-    static{
-        System.out.println("scan baseController");
-    }
+    protected Logger logger = Logger.getRootLogger();
 
 }
--- system/src/main/java/com/james/system/controller/SystemController.java
+++ system/src/main/java/com/james/system/controller/SystemController.java
@@ -2,15 +2,26 @@ package com.james.system.controller;
 
 import com.james.controller.base.controller.BaseController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by eronzen on 10/28/2016.
  */
 @Controller
+@RequestMapping("/sys")
 public class SystemController extends BaseController {
 
     static{
-        System.out.println("scan systemController");
+
+        System.out.print("--------------------");
+    }
+
+    @RequestMapping("/test")
+    public void test(){
+        logger.error("this is a test for error");
+        logger.warn("this is a test for warnning");
+        logger.info("this is a test for info");
+        logger.debug("this is a test for debug");
     }
 
 }
