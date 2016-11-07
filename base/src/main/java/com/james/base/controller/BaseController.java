5295cff9ddc05a8228e1035d0e613c0d003739ad
james <597162984@qq.com>
2016-11-07 21:03:04 +0800

    new branch

--- base/src/main/java/com/james/base/controller/BaseController.java
+++ base/src/main/java/com/james/base/controller/BaseController.java
@@ -7,4 +7,6 @@ import org.apache.log4j.Logger;
  */
 public class BaseController {
 
+
+
 }
---
+++ base/src/main/java/com/james/base/utils/Config.java
@@ -0,0 +1,7 @@
+package com.james.base.utils;
+
+/**
+ * Created by james` on 2016/11/7.
+ */
+public class Config {
+}
