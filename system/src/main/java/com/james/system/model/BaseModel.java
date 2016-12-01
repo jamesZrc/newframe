14510f6fe14762a23e4bb79f681a7d675835aea8
eronzen <597162984@qq.com>
2016-12-01 14:05:39 +0800

    add redis

--- system/src/main/java/com/james/system/model/BaseModel.java
+++ system/src/main/java/com/james/system/model/BaseModel.java
@@ -1,9 +1,11 @@
 package com.james.system.model;
 
+import java.io.Serializable;
+
 /**
  * Created by eronzen on 11/24/2016.
  */
-public class BaseModel {
+public class BaseModel implements Serializable {
 
     protected Long id;
 
