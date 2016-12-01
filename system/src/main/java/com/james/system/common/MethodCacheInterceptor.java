25ac065316294b74e27fd4e0daeae346a4f64276
eronzen <597162984@qq.com>
2016-12-01 15:00:31 +0800

    add redis

--- system/src/main/java/com/james/system/common/MethodCacheInterceptor.java
+++ system/src/main/java/com/james/system/common/MethodCacheInterceptor.java
@@ -4,46 +4,24 @@ import com.james.system.utils.Conf;
 import com.james.system.utils.RedisUtil;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
-import org.apache.log4j.Logger;
-
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by eronzen on 12/1/2016.
  */
 public class MethodCacheInterceptor implements MethodInterceptor {
-    private Logger logger = Logger.getLogger(MethodCacheInterceptor.class);
     private RedisUtil redisUtil;
     private List<String> targetNamesList; // 不加入缓存的service名称
     private List<String> methodNamesList; // 不加入缓存的方法名称
     private Long defaultCacheExpireTime; // 缓存默认的过期时间
-    private Long xxxRecordManagerTime; //
-    private Long xxxSetRecordManagerTime; //
 
     public MethodCacheInterceptor() {
         try {
-
-            String[] targetNames = Conf.get("targetNames").split(",");
-            String[] methodNames =  Conf.get("methodNames").split(",");
-
+            targetNamesList = Arrays.asList(Conf.get("targetNames").split(","));
+            methodNamesList = Arrays.asList(Conf.get("methodNames").split(","));
             // 加载过期时间设置
             defaultCacheExpireTime = Long.valueOf(Conf.get("defaultCacheExpireTime"));
-            xxxRecordManagerTime = Long.valueOf(Conf.get("com.service.impl.xxxRecordManager"));
-            xxxSetRecordManagerTime = Long.valueOf(Conf.get("com.service.impl.xxxSetRecordManager"));
-            // 创建list
-            targetNamesList = new ArrayList<String>(targetNames.length);
-            methodNamesList = new ArrayList<String>(methodNames.length);
-            Integer maxLen = targetNames.length > methodNames.length ? targetNames.length : methodNames.length;
-            // 将不需要缓存的类名和方法名添加到list中
-            for (int i = 0; i < maxLen; i++) {
-                if (i < targetNames.length) {
-                    targetNamesList.add(targetNames[i]);
-                }
-                if (i < methodNames.length) {
-                    methodNamesList.add(methodNames[i]);
-                }
-            }
         } catch (Exception e) {
             e.printStackTrace();
         }
